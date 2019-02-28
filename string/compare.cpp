cb9e9ba04e529e2ee28a3f4e3db8307d66d2721c
Mayjiao <Mayjiao@users.noreply.github.com>
2019-02-28 15:35:21 +0800

    Create compare.cpp

---
+++ string/compare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    string A("aBcdef");
+    string B("AbcdEf");
+    string C("123456");
+    string D("123dfg");
+    int m = A.compare(B);
+    int n = A.compare(1,5,B,4,2);
+    int p = C.compare(0,3,D,0,3);
+    cout << "m=" << m << "n=" << n <<  "p=" << p << endl;
+}
